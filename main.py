919296319fb5ad7ee8582cf8ebaa46813c75fe29
Luiz Filho <luiz.filho@credimorar.com.br>
2024-09-24 20:31:28 -0300

    Restore do arquivo main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+print ("jornada de dados")
+
+# Declarar variáveis
+nome = "Luiz Augusto"
+idade = 45
+
+# Print variáveis
+print (nome)
+
+# Calculo com variáveis
+def new_func(idade):
+    idade_nova = idade - 5
+    print (idade_nova)
+
+new_func(idade)
+
